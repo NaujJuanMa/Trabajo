0b2072a0c8672db221e4ad1b3cf64ed16cbd8493
Dell <juanmanuelmezarivera12@gmail.com>
2024-09-30 11:09:25 -0500

    Juan Manuel Meza

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Nombre: Juan Manuel Meza Rivera");
+        System.out.println("correo: Juan.meza-r@gmail.com\n");
+        System.out.println("direccion: kr 31#14-16 Zipaquira\n");
+        System.out.println("Hobbie: Jugar Futbol y ver peliculas\n");
+    }
+}
